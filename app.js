06c118cfd4e7313043f20f8cc764267a95370204
zarlishkotwal <you@example.com>
2021-11-20 18:34:43 +0500

    work complete

---
+++ app.js
@@ -0,0 +1,26 @@
+// alert("hello world");
+
+let outputScreen  = document.getElementById("output-screen");
+
+
+function display(num){
+    outputScreen.value += num;
+}
+function calculate(){
+    try {
+        outputScreen.value = eval(outputScreen.value); 
+    }
+    catch (err)
+{
+    alert("Invalid")
+  }
+
+}
+function clear (){
+    outputScreen.value = "";
+}
+function del(){
+    outputScreen.value = outputScreen.value.slice(0,-1);
+}
+
+
